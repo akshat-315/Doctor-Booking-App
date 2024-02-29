ed2a84418cc656d969d70413b46db19d11714117
akshat-315 <akshat315.sh@gmail.com>
2024-02-29 18:58:06 +0530

    Navigation Bar created

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   plugins: [require("tailwindcss"), require("autoprefixer")],
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx'],
 
   theme: {
     extend: {
